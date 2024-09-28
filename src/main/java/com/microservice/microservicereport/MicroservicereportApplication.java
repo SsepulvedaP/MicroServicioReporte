cb602d66827d933917eb14c9712c5614b8d1b1e8
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-09-28 15:47:52 -0500

    Creación del microservicio reporte

---
+++ src/main/java/com/microservice/microservicereport/MicroservicereportApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicereportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicereportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/microservicereport/MicroservicereportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicereportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
